e8631e7cd48308bb58973b5cf61fcde9d2d028fb
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2021-08-10 14:32:41 -0700

    Add _parseFormatValue() function

--- bad-investment-advice.js
+++ bad-investment-advice.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const BadInvestmentAdvice = (() => {
-  
+
   /* loadJSON(filepath): Loads the object stored in JSON file located at filepath */
   const _loadJSON = (filepath) => {
     /* Check to ensure file is of type .json */
@@ -31,6 +31,30 @@ const BadInvestmentAdvice = (() => {
   /* getRandomElement(array): Returns a random element of array */
   const _getRandomElement = (array) => {
     return array[Math.floor(Math.random() * array.length)];
+  }
+
+  /* parseFormatVlaue(string): Checks string to see if in valid format '{{value}}', returns object else null if not in format */
+  const _parseFormatValue = (string) => {
+    /* Checks to see if {{}} */
+    if(string.length >= 4 && (string[0] === string[1] && string[1] === '{') 
+    && (string[string.length - 1] === string[string.length - 2] && string[string.length - 1] === '}')){
+      const value = string.slice(2, string.length - 2);
+      /* Check to see if matches valid regex */
+      if(value.match(/[A-Za-z\.]/)){
+        /* Check to see if component has sub-values */
+        if(value.indexOf('.') !== -1){
+          return {
+            component: value.slice(0, value.indexOf('.')),
+            value: value.slice(value.indexOf('.') + 1)
+          }
+        }else{
+          return {
+            component: value
+          }
+        }
+      }
+    }
+    return null;
   }
 
   return {
